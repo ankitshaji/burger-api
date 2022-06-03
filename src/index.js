60bc5e7689112ed935dbd48e4c43bc997f47016a
Ankit Shaji <ankitshaji@gmail.com>
2022-06-03 20:07:50 +1200

    editData() - PUT Request

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@
 const beerDisplay = document.querySelector(".name");
 const button1 = document.querySelector(".button1");
 const button2 = document.querySelector(".button2");
+const button3 = document.querySelector(".button3");
 
 //GET - Request
 function getData() {
@@ -20,7 +21,7 @@ function getData() {
 
 //POST - Request
 function postData(data) {
-  fetch("https://my-burger-api.herokuapp.com/burgers", {
+  fetch("", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
@@ -29,16 +30,72 @@ function postData(data) {
   })
     .then((response) => response.json())
     .then((data) => {
-      console.log("Sucess: ", data);
-      beerDisplay.innerHTML = data;
+      console.log("Success:", data);
+      beerDisplay.innerHTML = data.name;
     })
     .catch((error) => {
-      console.error("Error: ", error);
-      beerDisplay.innerHTML = "Error occured";
+      console.error("Error:", error);
+      beerDisplay.innerHTML = "Error POST";
     });
 }
 
-const data = {
+//PUT - Requests
+function editData(data) {
+  fetch("", {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log("Success:", data);
+      beerDisplay.innerHTML = data.name;
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+      beerDisplay.innerHTML = "Error PUT";
+    });
+}
+//DELETE -Request
+function deleteData() {}
+
+//PUT-data
+const link2 = "https://my-burger-api.herokuapp.com/burgers/4";
+const data2 = {
+  id: 4,
+  name: "Crunchy Nacho Burger",
+  restaurant: "Max Burgers",
+  web: "https://www.max.se/maten/meny/burgare/crunchy-nacho-burger/",
+  description:
+    "The best combination of crunchiness and softness, all in one single burger",
+  ingredients: [
+    "sesame bun",
+    "salsa",
+    "cheddar",
+    "nachos",
+    "beef",
+    "tomato",
+    "pickled onion",
+    "lettuce",
+    "jalapeño mayonnaise",
+  ],
+  addresses: [
+    {
+      addressId: 0,
+      number: "123",
+      line1: "Skeppsbrogatan",
+      line2: "Luleå",
+      postcode: "971 25",
+      country: "Sweden",
+    },
+  ],
+};
+
+//POST-data
+const link1 = "https://my-burger-api.herokuapp.com/burgers";
+const data1 = {
   id: 28,
   name: "Mc Fly",
   restaurant: "Burger Burger",
@@ -66,4 +123,5 @@ const data = {
 };
 
 button1.addEventListener("click", getData);
-button2.addEventListener("click", postData(data));
+button2.addEventListener("click", postData(data1));
+button3.addEventListener("click", editData(data2));
