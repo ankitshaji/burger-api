de71084e5c908b9b27f598b22f69f13c17e67ecc
Ankit Shaji <ankitshaji@gmail.com>
2022-06-03 16:20:56 +1200

    postData() + post button

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 //random burger generator
 
 const beerDisplay = document.querySelector(".name");
-const button = document.querySelector(".button");
-//GET
+const button1 = document.querySelector(".button1");
+const button2 = document.querySelector(".button2");
+
+//GET - Request
 function getData() {
   fetch("https://my-burger-api.herokuapp.com/burgers")
     .then((response) => response.json())
@@ -16,4 +18,52 @@ function getData() {
     });
 }
 
-button.addEventListener("click", getData);
+//POST - Request
+function postData(data) {
+  fetch("https://my-burger-api.herokuapp.com/burgers", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log("Sucess: ", data);
+      beerDisplay.innerHTML = data;
+    })
+    .catch((error) => {
+      console.error("Error: ", error);
+      beerDisplay.innerHTML = "Error occured";
+    });
+}
+
+const data = {
+  id: 28,
+  name: "Mc Fly",
+  restaurant: "Burger Burger",
+  web: "https://burgerburger.co.nz/",
+  description:
+    "We believe in the power of great Kiwi hospitality to bring people together. We are a laid-back meeting place where friends and family can have great chats and bond in a space with character, with a burger or beersie in hand.",
+  ingredients: [
+    "fried chicken",
+    "bacon",
+    "swiss cheese",
+    "mayo",
+    "cos lettuce",
+    "bbq sauce",
+  ],
+  addresses: [
+    {
+      addressId: 0,
+      number: "3B",
+      line1: "York Street, Newmarket",
+      line2: "Brown Street, Ponsonby",
+      postcode: "1023",
+      country: "Auckland",
+    },
+  ],
+};
+
+button1.addEventListener("click", getData);
+button2.addEventListener("click", postData(data));
